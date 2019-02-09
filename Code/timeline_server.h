b7f5d0c30d971ae2c8d8fff8847ddbf2cd283abc
Marshall <marshall.hobbs96@tamu.edu>
2019-02-08 19:01:44 -0600

    a

--- Code/timeline_server.h
+++ Code/timeline_server.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <grpc++/grpc++.h>
 
+#define MAX_DATA 256
+
+enum IStatus
+{
+    SUCCESS,
+    FAILURE_ALREADY_EXISTS,
+    FAILURE_NOT_EXISTS,
+    FAILURE_INVALID_USERNAME,
+    FAILURE_INVALID,
+    FAILURE_UNKNOWN
+};
