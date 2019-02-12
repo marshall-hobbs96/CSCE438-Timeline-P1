da6320a46e303b1c10196f2116b42b4f77cee240
Marshall <marshall.hobbs96@tamu.edu>
2019-02-11 23:03:48 -0600

    compilable server, modified proto

--- Code/timeline_server.cpp
+++ Code/timeline_server.cpp
@@ -19,6 +19,7 @@ using grpc::ServerReader;
 using grpc::ServerReaderWriter;
 using grpc::ServerWriter;
 using grpc::Status;
+using grpc::StatusCode;
 
 using timeline::username;
 using timeline::fufArgs;
@@ -26,6 +27,7 @@ using timeline::Reply;
 using timeline::posts;
 using timeline::ListUsers;
 using timeline::Empty;
+using timeline::Timeline;
 using std::chrono::system_clock;
 
 
@@ -40,16 +42,15 @@ class clientEntry {
 
 };
 
-class timelineImpl final : public timeline::Service {
-
-    
-    std::vector<clientEntry> clients;
+class TimelineImpl final : public Timeline::Service {
 
     public:
 
+        std::vector<clientEntry> clients;
+
         Status acceptConnections(ServerContext * context, const username* username, Empty* response) override {       //great, we have a new user. So lets add it to clients. 
 
-            new clientEntry *newClient; 
+            clientEntry *newClient = new clientEntry; 
 
             newClient->clientName = username->user();
             clients.push_back(*newClient);
@@ -60,14 +61,15 @@ class timelineImpl final : public timeline::Service {
 
         Status follow(ServerContext * context, const fufArgs* args, Empty * empty) override {          //allow user to follow another user
 
-            if(args->clientName() == args->argName()) {  //someone is trying to follow themself... lets not waste time here...
+            if(args->clientname() == args->argname()) {  //someone is trying to follow themself... lets not waste time here...
 
-                return Status::INVALID_ARGUMENT;
+
+                return Status(StatusCode::INVALID_ARGUMENT, "You can't follow yourself");
 
             }
 
-            std::string clientName = args->clientName();
-            std::string followName = args->argName();
+            std::string clientName = args->clientname();
+            std::string followName = args->argname();
 
             int j = 0;
             int client = -1;
@@ -92,13 +94,13 @@ class timelineImpl final : public timeline::Service {
 
             if(client == -1) {  //shouldn't happen, but means that we couldn't find the client making request in our database
 
-                return Status::INVALID_ARGUMENT;
+                return Status(StatusCode::INVALID_ARGUMENT, "We couldn't find you in our database");
 
             }
 
             else if(follow == -1) { //we couldn't find the username of the user we're trying to follow in our database
 
-                return Status::NOT_FOUND;
+                return Status(StatusCode::NOT_FOUND, "We couldn't find the user you were looking for");
 
             }
 
@@ -115,14 +117,14 @@ class timelineImpl final : public timeline::Service {
 
         Status unfollow(ServerContext* context, const fufArgs * args, Empty * empty) override {
 
-            if(args->clientName() == args->argName()) {       //someone is trying to unfollow themself...again, lets not waste time here...
+            if(args->clientname() == args->argname()) {       //someone is trying to unfollow themself...again, lets not waste time here...
 
-                return Status::INVALID_ARGUMENT;
+                return Status(StatusCode::INVALID_ARGUMENT, "You can't unfollow yourself");
 
             }
 
-            std::string clientName = args->clientName();
-            std::string followName = args->argName();
+            std::string clientName = args->clientname();
+            std::string followName = args->argname();
             int j = 0;
             int client = -1;
             int unfollow = -1;
@@ -145,19 +147,19 @@ class timelineImpl final : public timeline::Service {
 
             if(client == -1) {  //shouldn't happen, but means that we couldn't find the client making request in our database
 
-                return Status::INVALID_ARGUMENT;
+                return Status(StatusCode::INVALID_ARGUMENT, "We couldn't find you in our database");
 
             }
 
             else if(unfollow == -1) { //we couldn't find the username of the user we're trying to unfollow in our database
 
-                return Status::NOT_FOUND;
+                return Status(StatusCode::NOT_FOUND, "We couldn't find the user you were looking for");
 
             }
 
             else {          //good, we found the client making the argument, and we found the user we're trying to unfollow ////////////////////////////////////////////////////////////////////
 
-                clients.at(client).followedList.erase(unfollow); 
+                clients.at(client).followedList.erase(clients.at(client).followedList.begin() + unfollow - 1); 
                 
                 return Status::OK;
 
@@ -167,16 +169,16 @@ class timelineImpl final : public timeline::Service {
 
         }
 
-        Status List(ServerContext * context, const Empty * empty, ServerWriter<ListUsers> * writer) override {   //list out all the names of the users. Easy
+        Status list(ServerContext * context, const Empty * empty, ServerWriter<ListUsers> * writer) override {   //list out all the names of the users. Easy
 
             int j = 0;
 
             for(j = 0; j < clients.size(); j++) {
 
                 ListUsers * newUser = new ListUsers;
-                *newUser->set_user(clients.at(j));
-                writer->Write(*user);
-                delete user;
+                newUser->set_user(clients.at(j).clientName);
+                writer->Write(*newUser);
+                delete newUser;
 
             }
 
@@ -184,7 +186,7 @@ class timelineImpl final : public timeline::Service {
 
         }
 
-        Status updateTimeline(ServerContext * context, const username * user, ServerWriter<posts> writer) override {
+        Status updateTimeline(ServerContext * context, const username * user, ServerWriter<posts>* writer) override {
 
             std::string connected_client = user->user();
             int client_place = -1; 
@@ -203,14 +205,14 @@ class timelineImpl final : public timeline::Service {
 
             if(client_place == -1) {
 
-                return Status::FAILED_PRECONDITION;
+                return Status(StatusCode::FAILED_PRECONDITION, "We couldn't find you in our database");
 
             }
 
             int l = 0;
             for(l = 0; ((l < 20) && (l < clients.at(client_place).timeline.size())); l++) {
 
-                writer->Write(&clients.at(client_place).timeline.at(time.size() - l - 1));
+                writer->Write((clients.at(client_place).timeline.at(clients.at(client_place).timeline.size() - l - 1)));
 
             }
 
@@ -227,7 +229,7 @@ class timelineImpl final : public timeline::Service {
 
             for(j = 0; j < clients.size(); j++){
 
-                if(connected_client == clients.at(j).clientName) {
+                if(client_name == clients.at(j).clientName) {
 
                     client_place = j; 
 
@@ -237,22 +239,22 @@ class timelineImpl final : public timeline::Service {
 
             if(client_place == -1) {
 
-                return Status::FAILED_PRECONDITION;
+                return Status(StatusCode::FAILED_PRECONDITION, "We couldn't find you in our database");
 
             }
 
-            clients.at(client_place).client_posts.push_back(post);
-            clients.at(client_place).timeline.push_back(post);
+            clients.at(client_place).client_posts.push_back(*post);
+            clients.at(client_place).timeline.push_back(*post);
 
-            int j = 0;
+            j = 0;
             for(j = 0; j < clients.size(); j++){
 
                 int k = 0;
                 for(k = 0; k < clients.at(j).followedList.size(); k ++){
 
-                    if(clients.at(j).followedList.at(k) == connected_client){
+                    if(clients.at(j).followedList.at(k) == client_name){
 
-                        clients.at(j).timeline.push_back(post);
+                        clients.at(j).timeline.push_back(*post);
 
                     }
 
@@ -333,13 +335,13 @@ class timelineImpl final : public timeline::Service {
 void runServer(const std::string host, const std::string port) {
 
     std::string serverAddress = host + port;
-    timelineServiceImpl service;
+    TimelineImpl service;
 
     ServerBuilder builder;
-    builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials);
+    builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << serverAddress << std::endl;
-    server->wait();
+    server->Wait();
 
 };
