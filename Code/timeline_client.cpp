f1d3760f34e86605ece023a8dd11ce1f33607bae
Marshall <marshall.hobbs96@tamu.edu>
2019-02-12 11:23:30 -0600

    Merged timeline_client

--- Code/timeline_client.cpp
+++ Code/timeline_client.cpp
@@ -7,35 +7,53 @@
 #include <sstream>
 
 #include <unistd.h>
-#include <grpc++/grpc++.h>
+
+#include <grpc/grpc.h>
+#include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+
+//#include "helper.h"
+
 #include "timeline_client.h"
+#include "timeline.grpc.pb.h"
+
+#include <chrono>
+#include <ctime>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
 
 class Client : public IClient
 {
     public:
         Client(const std::string& hname,
-               const std::string& uname,
-               const std::string& p)
-            :hostname(hname), username(uname), port(p)
-            {}
+            const std::string& uname,
+            const std::string& p)
+            :hostname(hname), myUsername(uname), port(p){}
     protected:
         virtual int connectTo();
         virtual IReply processCommand(std::string& input);
         virtual void processTimeline();
     private:
         std::string hostname;
-        std::string username;
+        std::string myUsername;
         std::string port;
         
         // You can have an instance of the client stub
         // as a member variable.
-        //std::unique_ptr<NameOfYourStubClass::Stub> stub_;
+        std::unique_ptr<timeline::Stub> stub_;
 };
 
 int main(int argc, char** argv) {
 
     std::string hostname = "localhost";
-    std::string username = "default";
+    std::string myUsername = "default";
     std::string port = "3010";
     int opt = 0;
     while ((opt = getopt(argc, argv, "h:u:p:")) != -1){
@@ -43,7 +61,7 @@ int main(int argc, char** argv) {
             case 'h':
                 hostname = optarg;break;
             case 'u':
-                username = optarg;break;
+                myUsername = optarg;break;
             case 'p':
                 port = optarg;break;
             default:
@@ -51,7 +69,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    Client myc(hostname, username, port);
+    Client myc(hostname, myUsername, port);
     // You MUST invoke "run_client" function to start business logic
     myc.run_client();
 
@@ -60,69 +78,83 @@ int main(int argc, char** argv) {
 
 int Client::connectTo()
 {
-	// ------------------------------------------------------------
-    // In this function, you are supposed to create a stub so that
-    // you call service methods in the processCommand/porcessTimeline
-    // functions. That is, the stub should be accessible when you want
-    // to call any service methods in those functions.
-    // I recommend you to have the stub as
-    // a member variable in your own Client class.
-    // Please refer to gRpc tutorial how to create a stub.
-	// ------------------------------------------------------------
-	string host = this.hostname + ":" + this.host;
-	grpc::CreateChannel(host, grpc::InsecureChannelCredentials());
+    std::string hosting = this->hostname + ":" + this->port;
+    stub_ = timeline::NewStub(grpc::CreateChannel(hosting, grpc::InsecureChannelCredentials()));
+
+    ClientContext context;
+    username usr;
+    Reply reply;
+
+    usr.set_user(this->myUsername);
+    grpc::Status status = stub_->acceptConnections(&context, usr, &reply);
+
+    //Set up default message
+    //init_post.set_user(myUsername);	
+    //init_post.set_time(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+    //init_post.set_post("timeline created");
+
+    //Writes the post there
+    //std::shared_ptr<ClientReaderWriter<::posts, ::posts>> swr(stub_->timeline(&context));
+    //swr->Write(init_post);
+    //swr->WritesDone();
+
     return 1; // return 1 if success, otherwise return -1
 }
 
 IReply Client::processCommand(std::string& input)
 {
-	// ------------------------------------------------------------
-	// GUIDE 1:
-	// In this function, you are supposed to parse the given input
-    // command and create your own message so that you call an 
-    // appropriate service method. The input command will be one
-    // of the followings:
-	//
-	// FOLLOW <username>
-	// UNFOLLOW <username>
-	// LIST
-    // TIMELINE
-	//
-	// - JOIN/LEAVE and "<username>" are separated by one space.
-	// ------------------------------------------------------------
-	
-	std::string str("Split me by whitespaces");
-    std::string buf;                 // Have a buffer string
-    std::stringstream ss(str);       // Insert the string into a stream
-    std::vector<std::string> tokens; // Create vector to hold our words
-
-    while (ss >> buf)
+    //Split tokens into the vector
+    std::string buf;
+    std::stringstream ss(input);
+    std::vector<std::string> tokens;
+    while (ss >> buf){
         tokens.push_back(buf);
+    }
+
+    //Set up variables here
+    ClientContext context;    //Client that's being sent replies
+    grpc::Status status;      //Success or Fail	
+    Reply reply;              //Some possible reply
+    fufArgs usrs;             //Username1 and Username2
+    IReply ire;               //Their custom reply
 	
-	if(strstr(tokens[0].c_str(),"FOLLOW".cstr)) {
-	  
-	}
-	else if(strstr(tokens[0].c_str(),"UNFOLLOW".cstr)) {
-	  
-	}
-	else if(strstr(tokens[0].c_str(),"LIST".cstr)) {
-	  
-	}
-	else if(strstr(tokens[0].c_str(),"TIMELINE".cstr)) {
-	  
-	}
-	else {
-	  
-	}
+    if(strstr(tokens[0].c_str(),"FOLLOW")) {
+        usrs.set_clientname(this->myUsername);
+        usrs.set_argname(tokens[1]);
+        status = stub_->follow(&context, usrs, &reply);
+        //std::cout<<"reply:"<<reply.status();
+    }
+    else if(strstr(tokens[0].c_str(),"UNFOLLOW")) {
+        usrs.set_clientname(this->myUsername);
+        usrs.set_argname(tokens[1]);
+        status = stub_->follow(&context, usrs, &reply);
+        //std::cout<<"reply:"<<reply.status();
+    }
+    else if(strstr(tokens[0].c_str(),"LIST")) {
+        ListUsers list;
+        Empty empty;
+        status = stub_->list(&context, empty, &list);
+        //std::cout<<"list:"<<std::endl;
+        //std::unique_ptr<ClientReader<ListUsers> > reader(stub_->list(&context,empty));
+        //while (reader->Read(&usr)) {           
+        //    std::cout <<usr.user()<< std::endl;
+        //    ire.all_users.push_back(usr.user());
+        //}
+        //Status status = reader->Finish();
+    }
+    else if(strstr(tokens[0].c_str(),"TIMELINE")) {
+        //processtimeline()
+    }
+    else {}
     // ------------------------------------------------------------
-	// GUIDE 2:
-	// Then, you should create a variable of IReply structure
-	// provided by the client.h and initialize it according to
-	// the result. Finally you can finish this function by returning
+    // GUIDE 2:
+    // Then, you should create a variable of IReply structure
+    // provided by the client.h and initialize it according to
+    // the result. Finally you can finish this function by returning
     // the IReply.
-	// ------------------------------------------------------------
+    // ------------------------------------------------------------
     
-	// ------------------------------------------------------------
+    // ------------------------------------------------------------
     // HINT: How to set the IReply?
     // Suppose you have "Join" service method for JOIN command,
     // IReply can be set as follow:
@@ -143,9 +175,15 @@ IReply Client::processCommand(std::string& input)
     // IMPORTANT: 
     // For the command "LIST", you should set both "all_users" and 
     // "following_users" member variable of IReply.
-	// ------------------------------------------------------------
+    // ------------------------------------------------------------
     
-    IReply ire;
+   
+    ire.grpc_status = status;
+    if (status.ok())
+        ire.comm_status = SUCCESS;
+    else
+        ire.comm_status = FAILURE_NOT_EXISTS;
+
     return ire;
 }
 
